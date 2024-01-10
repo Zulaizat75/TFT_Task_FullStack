c1b55dfed2d2a8afb381f4690c0bc4b9b5a1f3d2
Zulaizat75 <m.zulaizat96@gmail.com>
2024-01-10 23:30:41 +0800

    D12 Task 1
    Day 12 Task 1 Upload

---
+++ Day 12 - Task 1 - Vue.js Tasklist/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <h1>Task Adder App</h1>
+    <input placeholder="Add todo" @keyup.enter="addTodo" v-model="newTodo" /> <br>
+    <!-- Add a button to name "add task" -->
+    <button @click="addTodo">Add Task</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      newTodo: "",
+      todos: [],
+    };
+  },
+  methods:{
+    addTodo(){
+      if(this.newTodo.trim() !== ""){
+        this.todos.push(this.newTodo.trim());
+        this.newTodo = "";
+      }
+    },
+  },
+};
+</script>
+
+<!-- code css -->
+<style>
+body{
+  background-color: bisque;
+}
+#app {
+  text-align: center;
+  margin-top: 3rem;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  background-color: white;
+}
+input {
+  padding: 1rem;
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+  width: 60%;
+  border-radius: 0.25rem;
+}
+button{
+  padding: 1rem;
+  font-size: 1rem;  
+  border-radius: 0.25rem;
+}
+button:hover{
+  cursor: pointer;
+  background-color: azure;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+}
+li button:hover{
+  background-color: bisque;
+}
+</style>
